44fadcfb6ff409c3788f64303a747851849ce817
mkj4489 <masakijo@gmail.com>
2019-02-10 22:15:36 +0900

    npm install

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 'use strict';
+const request = require('request');
+request('http://www.google.com', (error, response, body) => {
+    //console.log(body);
+    console.log(response.statusCode);
+});
